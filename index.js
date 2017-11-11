c1ca5684e4ce175e167bdaa0c314431d59467344
Puranam Pradeep Picasso <pichupicasso@gmail.com>
2017-11-12 04:15:06 +0530

    finioshed auth load

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+
+
+//Order should be this way, model should be declared first and passport use auth taking mongoose.model from model
+require('./models/User');
+require('./services/passport');
+
+
+//Mongoose connection
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send({Hi : 'There'});
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>{
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT, () => {
     console.log(`The app is running on port of ${PORT}`);
 });
